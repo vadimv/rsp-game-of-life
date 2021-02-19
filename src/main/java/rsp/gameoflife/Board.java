ff2b13eb7f9b9dc41e0e8538f633e03d305bf364
Vadim Vashkevich <vadim.vashkevich@gmail.com>
2021-02-19 18:03:29 +0100

    Refactor

--- src/main/java/rsp/gameoflife/Board.java
+++ src/main/java/rsp/gameoflife/Board.java
@@ -3,49 +3,52 @@ package rsp.gameoflife;
 public class Board {
     public static final int HEIGHT = 50;
     public static final int WIDTH = 100;
+    private static final int LENGTH = HEIGHT * WIDTH;
 
-    public final Cell[][] cells;
+    public final boolean[] cells;
 
-    private Board(Cell[][] cells) {
+    private Board(boolean[] cells) {
         this.cells = cells;
     }
 
     public static Board create() {
-        final Cell[][] c = new Cell[HEIGHT][WIDTH];
-        for(int y = 0; y < HEIGHT; y++) {
-            for(int x = 0; x < WIDTH; x++) {
-                c[y][x] = new Cell(x, y, false);
-            }
-        }
-        return new Board(c);
+        return new Board(new boolean[HEIGHT * WIDTH]);
+    }
+
+    public static int x(int index) {
+        return index % WIDTH;
+    }
+
+    public static int y(int index) {
+        return index / WIDTH;
+    }
+
+    public static int index(int y, int x) {
+        return (y * WIDTH) + (x % WIDTH);
     }
 
     public Board setActive(int x, int y, boolean a) {
-        final Cell[][] copy = new Cell[cells.length][cells[0].length];
-        for (int i=0;i < cells.length;i++) {
-            for(int j=0;j < cells[0].length;j++){
-                copy[i][j] = i == y && j == x && cells[y][x].active != a ? new Cell(x, y, a) : cells[i][j];
-            }
-        }
+        final boolean[] copy = cells.clone();
+        copy[index(y, x)] = a;
         return new Board(copy);
     }
 
     public Board toggle(int x, int y) {
-        return setActive(x, y, !cells[y][x].active);
+        return setActive(x, y, !cells[index(y, x)]);
     }
 
     public Board advance() {
-        final Cell[][] copy = new Cell[cells.length][cells[0].length];
+        final boolean[] copy = new boolean[cells.length];
         for(int y = 0; y < HEIGHT; y++) {
             for(int x = 0; x < WIDTH; x++) {
                 final int n = neighbours(x, y);
-                final Cell cell = cells[y][x];
-                if (cell.active) {
-                    if (n < 2 || n > 3) copy[y][x] = new Cell(x, y, false);
-                        else copy[y][x] = cells[y][x];
+                final int i = index(y, x);
+                if (cells[index(y, x)]) {
+                    if (n < 2 || n > 3) copy[i] = false; // the cell dies
+                        else copy[i] = cells[i];
                 } else {
-                    if (n == 3) copy[y][x] = new Cell(x, y, true);
-                        else copy[y][x] = cells[y][x];
+                    if (n == 3) copy[i] = true; // becomes a live cell
+                        else copy[i] = cells[i];
                 }
             }
         }
@@ -59,30 +62,19 @@ public class Board {
         final int leftX = x - 1 < 0 ? (WIDTH - 1) : x - 1;
         final int rightX = (x + 1 == WIDTH) ? 0 : x + 1;
 
-        return b2i(cells[topY][leftX].active)
-                + b2i(cells[topY][x].active)
-                + b2i(cells[topY][rightX].active)
-                + b2i(cells[y][leftX].active)
-                + b2i(cells[y][rightX].active)
-                + b2i(cells[bottomY][leftX].active)
-                + b2i(cells[bottomY][x].active)
-                + b2i(cells[bottomY][rightX].active);
+        return b2i(cells[index(topY, leftX)])
+                + b2i(cells[index(topY, x)])
+                + b2i(cells[index(topY, rightX)])
+                + b2i(cells[index(y, leftX)])
+                + b2i(cells[index(y, rightX)])
+                + b2i(cells[index(bottomY, leftX)])
+                + b2i(cells[index(bottomY, x)])
+                + b2i(cells[index(bottomY, rightX)]);
     }
 
     private static int b2i(boolean value) {
         return value ? 1 : 0;
     }
 
-    public static class Cell {
-        public final int x;
-        public final int y;
 
-        public final boolean active;
-
-        public Cell(int x, int y, boolean active) {
-            this.x = x;
-            this.y = y;
-            this.active = active;
-        }
-    }
 }
--- src/main/java/rsp/gameoflife/Life.java
+++ src/main/java/rsp/gameoflife/Life.java
@@ -4,52 +4,56 @@ import rsp.App;
 import rsp.Component;
 import rsp.jetty.JettyServer;
 import rsp.server.StaticResources;
+import rsp.util.data.Tuple2;
 
 import java.io.File;
-import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static rsp.dsl.Html.*;
 
+/**
+ * An implementation of Conway's Game of Life.
+ */
 public class Life {
 
     public static void main(String[] args) throws Exception {
-        final Map<String, ScheduledFuture<?>> timers = new ConcurrentHashMap<>();
         final Component<State> component = useState ->
                 html(head(link(attr("rel", "stylesheet"), attr("href","/res/style.css"))),
                      body(div(attr("class", "tetris-wrapper"),
                              div(attr("class", "board"),
-                                     of(Arrays.stream(useState.get().board.cells).flatMap(row ->
-
-                                             Arrays.stream(row).map(cell ->
-                                                     div(attr("class", "cell t" + (cell.active ? "J" : "0")),
+                                     of(cellsStreamWithIndices(useState.get().board.cells)
+                                             .map(cell ->
+                                                     div(attr("class", "cell t" + (cell._1 ? "J" : "0")),
                                                          on("click", c -> {
-                                                            System.out.println("Clicked x=" + cell.x + " y=" + cell.y);
-                                                            useState.accept(useState.get().toggleCell(cell.x, cell.y));
-                                                     }))))))),
+                                                            System.out.println("Clicked x=" + Board.x(cell._2) + " y=" + Board.y(cell._2));
+                                                            useState.accept(useState.get().toggleCell(Board.x(cell._2), Board.y(cell._2)));
+                                                     })))))),
                              div(attr("class", "controls"),
                                      button(attr("id", "start-btn"), attr("type", "button"),
                                              when(false, () -> attr("disabled")),
                                              text("Start"), on("click", c -> {
                                                      System.out.println("Start");
-
-                                                 //timers.put(c.sessionId().sessionId,
-                                                         c.scheduleAtFixedRate(() -> useState.accept(s -> s.advance()),
-                                                                          0, 200, TimeUnit.MILLISECONDS);
-                                                 //);
-
+                                                     c.scheduleAtFixedRate(() -> useState.accept(s -> s.advance()),
+                                                                      0, 200, TimeUnit.MILLISECONDS);
                                              })))));
+
         final var initialState = State.initialState();
         final var s = new JettyServer(8080,
-                "",
-                new App(initialState,
-                        component),
-                new StaticResources(new File("src/main/java/rsp/examples/life"),
-                        "/res/*"));
+                                      "",
+                                       new App(initialState,
+                                               component),
+                                       new StaticResources(new File("src/main/java/rsp/gameoflife"),
+                                                          "/res/*"));
         s.start();
         s.join();
+    }
+
+    private static Stream<Tuple2<Boolean, Integer>> cellsStreamWithIndices(boolean[] cells) {
+        return IntStream.range(0, cells.length).mapToObj(idx -> new Tuple2<>(cells[idx], idx));
     }
 }
