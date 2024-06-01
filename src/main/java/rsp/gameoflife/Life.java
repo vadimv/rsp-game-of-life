e1d763e2af91f43478faaa4524e7e11afe639a70
Vadim Vashkevich <vadim.vashkevich@gmail.com>
2024-06-01 09:51:02 +0200

    Upgrade to rsp 2.0

--- src/main/java/rsp/gameoflife/Life.java
+++ src/main/java/rsp/gameoflife/Life.java
@@ -1,14 +1,14 @@
 package rsp.gameoflife;
 
 import rsp.App;
-
-import rsp.jetty.JettyServer;
-import rsp.ref.TimerRef;
+import rsp.component.*;
+import rsp.jetty.WebServer;
 import rsp.server.StaticResources;
-import rsp.stateview.ComponentView;
 
 import java.io.File;
-import java.util.concurrent.TimeUnit;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.*;
 import java.util.stream.IntStream;
 
 import static rsp.html.HtmlDsl.*;
@@ -19,63 +19,110 @@ import static rsp.html.HtmlDsl.*;
  */
 public class Life {
     private static final int NEXT_GENERATION_DELAY_MS = 200;
-    private static final TimerRef TIMER_REF = TimerRef.createTimerRef();
 
     public static void main(String[] args) {
-        final ComponentView<State> component = state -> newState -> {
-            final var cells = state.board.cells;
-            return html(head(title("Conway's Game of Life"),
-                            link(attr("rel", "stylesheet"),
-                                    attr("href", "/res/style.css"))),
-                    body(div(attr("class", "tetris-wrapper"),
-                                    div(attr("class", "board"),
-                                            of(IntStream.range(0, cells.length)
-                                                    .mapToObj(index ->
-                                                            div(attr("class", "c" + (cells[index] ? "1" : "0")),
-                                                                    on("click", c -> {
-                                                                        System.out.println("Clicked x=" + Board.x(index) + " y=" + Board.y(index));
-                                                                        newState.set(state.toggleCell(Board.x(index), Board.y(index)));
-                                                                    })))))),
-                            div(attr("class", "controls"),
-                                    button(attr("type", "button"),
-                                            when(state.isRunning, () -> attr("disabled")),
-                                            text("Start"),
-                                            on("click", c -> {
-                                                System.out.println("Start");
-                                                newState.apply(s -> s.setIsRunning(true));
-                                                c.scheduleAtFixedRate(() -> newState.apply(State::advance),
-                                                        TIMER_REF,0, NEXT_GENERATION_DELAY_MS, TimeUnit.MILLISECONDS);
-                                            })),
-                                    button(attr("type", "button"),
-                                            when(!state.isRunning, () -> attr("disabled")),
-                                            text("Stop"),
-                                            on("click", c -> {
-                                                System.out.println("Stop");
-                                                c.cancelSchedule(TIMER_REF);
-                                                newState.apply(s -> s.setIsRunning(false));
-                                            })),
-                                    button(attr("type", "button"),
-                                            when(state.isRunning, () -> attr("disabled")),
-                                            text("Clear"),
-                                            on("click", c -> {
-                                                System.out.println("Clear");
-                                                newState.apply(s -> State.initialState());
-                                            })),
-                                    button(attr("type", "button"),
-                                            when(state.isRunning, () -> attr("disabled")),
-                                            text("Random"),
-                                            on("click", c -> {
-                                                System.out.println("Random");
-                                                newState.apply(s -> State.initialState(true));
-                                            })))));
+        final StatefulComponentDefinition<State> componentDefinition = new StatefulComponentDefinition<>(Life.class) {
+
+            private final ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(8);
+            private final Map<Object, ScheduledFuture<?>> schedules = new HashMap<>();
+            @Override
+            protected ComponentStateSupplier<State> stateSupplier() {
+                return (key, httpStateOrigin) -> CompletableFuture.completedFuture(State.initialState());
+            }
+
+            @Override
+            protected ComponentView<State> componentView() {
+                return state -> newState -> {
+                    final var cells = state.board.cells;
+                    return html(head(title("Conway's Game of Life"),
+                                    link(attr("rel", "stylesheet"),
+                                            attr("href", "/res/style.css"))),
+                            body(div(attr("class", "tetris-wrapper"),
+                                            div(attr("class", "board"),
+                                                    of(IntStream.range(0, cells.length)
+                                                            .mapToObj(index ->
+                                                                    div(attr("class", "c" + (cells[index] ? "1" : "0")),
+                                                                            on("click", c -> {
+                                                                                System.out.println("Clicked x=" + Board.x(index) + " y=" + Board.y(index));
+                                                                                newState.setState(state.toggleCell(Board.x(index), Board.y(index)));
+                                                                            })))))),
+                                    div(attr("class", "controls"),
+                                            button(attr("type", "button"),
+                                                    when(state.isRunning, () -> attr("disabled")),
+                                                    text("Start"),
+                                                    on("click", c -> {
+                                                        System.out.println("Start");
+                                                        newState.applyStateTransformation(s -> s.setIsRunning(true));
+                                                    })),
+                                            button(attr("type", "button"),
+                                                    when(!state.isRunning, () -> attr("disabled")),
+                                                    text("Stop"),
+                                                    on("click", c -> {
+                                                        System.out.println("Stop");
+                                                        newState.applyStateTransformation(s -> s.setIsRunning(false));
+                                                    })),
+                                            button(attr("type", "button"),
+                                                    when(state.isRunning, () -> attr("disabled")),
+                                                    text("Clear"),
+                                                    on("click", c -> {
+                                                        System.out.println("Clear");
+                                                        newState.applyStateTransformation(s -> State.initialState());
+                                                    })),
+                                            button(attr("type", "button"),
+                                                    when(state.isRunning, () -> attr("disabled")),
+                                                    text("Random"),
+                                                    on("click", c -> {
+                                                        System.out.println("Random");
+                                                        newState.applyStateTransformation(s -> State.initialState(true));
+                                                    })))));
+                };
+            }
+
+            @Override
+            protected ComponentUpdatedCallback<State> componentDidUpdate() {
+                return (key, oldState, state, newState) -> {
+                    if (!oldState.isRunning && state.isRunning) {
+                        scheduleAtFixedRate(() -> newState.applyStateTransformation(State::advance),
+                                            key,
+                                            0,
+                                            NEXT_GENERATION_DELAY_MS,
+                                            TimeUnit.MILLISECONDS);
+                    } else if (oldState.isRunning && !state.isRunning) {
+                        cancelSchedule(key);
+                    }
+                };
+            }
+
+            private void scheduleAtFixedRate(final Runnable command, final Object key, final long initialDelay, final long period, final TimeUnit unit) {
+                final ScheduledFuture<?> timer = scheduledExecutorService.scheduleAtFixedRate(command, initialDelay, period, unit);
+                schedules.put(key, timer);
+            }
+
+            @Override
+            protected ComponentUnmountedCallback<State> componentWillUnmount() {
+                return (key, state) -> {
+                    cancelSchedule(key);
+                };
+            }
+
+            private void cancelSchedule(final Object key) {
+                final ScheduledFuture<?> schedule = schedules.get(key);
+                if (schedule != null) {
+                    schedule.cancel(true);
+                    schedules.remove(key);
+                }
+            }
+
+            @Override
+            protected ComponentMountedCallback<State> componentDidMount() {
+                return (key, state, newState) -> {};
+            }
         };
-        final var initialState = State.initialState();
-        final var s = new JettyServer<>(8080,
-                "",
-                new App<>(initialState,
-                        component),
-                new StaticResources(new File("src/main/java/rsp/gameoflife"),
-                        "/res/*"));
+
+        final var s = new WebServer(8080,
+                                      new App<>(componentDefinition),
+                                      new StaticResources(new File("src/main/java/rsp/gameoflife"),
+                                                         "/res/*"));
         s.start();
         s.join();
     }
